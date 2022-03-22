a0d75f265b9e4246fc1671ffc66169fa33e8eadd
thetarnav <gthetarnav@gmail.com>
2022-03-22 21:25:11 +0100

    update

--- lib_1/vite.config.ts
+++ lib_1/vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+  test: {
+    clearMocks: true,
+    environment: "jsdom",
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
+    deps: {
+      inline: [/solid-js/],
+    },
+  },
   resolve: {
     conditions: ["development", "browser"],
   },
