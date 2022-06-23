877e835f21ac33a1b21c0117890fd36d28fa075a
Damian Tarnawski <gthetarnav@gmail.com>
2022-06-23 17:59:22 +0200

    try aliasing

--- lib_1/vite.config.ts
+++ lib_1/vite.config.ts
@@ -1,6 +1,43 @@
 import { defineConfig } from "vitest/config"
+import { resolve } from "path"
+import alias from "@rollup/plugin-alias"
+const { importMaps } = require("vite-plugin-import-maps")
+
+const paths = {
+  "solid-js": resolve(__dirname, "node_modules/solid-js/dist/solid.js"),
+  "solid-js/web": resolve(__dirname, "node_modules/solid-js/web/dist/web.js"),
+  "solid-js/store": resolve(__dirname, "node_modules/solid-js/store/dist/store.js"),
+}
+
+const x = {
+  entries: [
+    {
+      find: "solid-js",
+      replacement: resolve(__dirname, "node_modules/solid-js/dist/solid.js"),
+    },
+    {
+      find: "solid-js/web",
+      replacement: resolve(__dirname, "node_modules/solid-js/web/dist/web.js"),
+    },
+    {
+      find: "solid-js/store",
+      replacement: resolve(__dirname, "node_modules/solid-js/store/dist/store.js"),
+    },
+  ],
+}
 
 export default defineConfig({
+  // plugins: [
+  //   importMaps([
+  //     {
+  //       imports: {
+  //         "solid-js/web": "solid-js/web/dist/web.js",
+  //         "solid-js/store": "solid-js/store/dist/store.js",
+  //         "solid-js": "solid-js/dist/solid.js",
+  //       },
+  //     },
+  //   ]),
+  // ],
   test: {
     clearMocks: true,
     environment: "jsdom",
@@ -12,6 +49,20 @@ export default defineConfig({
     },
   },
   resolve: {
+    alias: [
+      {
+        find: "solid-js/web",
+        replacement: resolve(__dirname, "node_modules/solid-js/web/dist/web.js"),
+      },
+      {
+        find: "solid-js/store",
+        replacement: resolve(__dirname, "node_modules/solid-js/store/dist/store.js"),
+      },
+      {
+        find: "solid-js",
+        replacement: resolve(__dirname, "node_modules/solid-js/dist/solid.js"),
+      },
+    ],
     conditions: ["development", "browser"],
   },
 })
